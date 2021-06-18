2fc630b79d1be6ceabf3811d0f9c663f0752da0a
Argzon <argzon01@gmail.com>
2021-06-19 01:49:15 +0200

    Added error page

---
+++ src/components/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+	return (
+		<div className="page-container">
+        	<h1 className="title">The page you were looking for cannot be found</h1>
+      	</div>
+	)
+};
+
+export default ErrorPage;
