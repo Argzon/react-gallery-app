8cc30a4df5c342572b1c90fc8ef3b2e2b78e565a
Argzon <argzon01@gmail.com>
2021-06-19 01:36:36 +0200

    Added Search Function

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component }from 'react';
 import axios from 'axios';
 import apiKey from './config';
+import { 
+    BrowserRouter as Router,
+} from 'react-router-dom';
 
+// Components import
 import ImgList from './components/ImgList';
+import Nav from './components/Nav';
+import SearchForm from './components/SearchForm';
+import NotFound from './components/NotFound';
 
 export default class App extends Component {
     state = {
-        images: []
+        images: [],
+        loading: true
     }
 
     componentDidMount() {
         this.performSearch();
     }
 
-    performSearch = (query = 'sunsets') =>{
+    performSearch = (query = 'waterfall') =>{
         axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${query}&per_page=24&format=json&nojsoncallback=1`)
             .then(res => {
                 const images = res.data.photos.photo;
-                this.setState({images});
+                this.setState({
+                    images,
+                    loading: false
+                });
             })
     }
 
     render() {
         return(
             <div className="container">
-                <ImgList data={this.state.images} />
+                <SearchForm onSearch={this.performSearch} />
+                <Router>
+                    <Nav />
+                </Router>
+                { (this.state.loading) ? <NotFound /> : <ImgList data={this.state.images} /> }
             </div>
         );
     }
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Nav = () => (
+    <nav className="main-nav">
+        <ul>
+          <li><NavLink to='/cats'>Cats</NavLink></li>
+          <li><NavLink to='/dogs'>Dogs</NavLink></li>
+          <li><NavLink to='/computers'>Computers</NavLink></li>
+        </ul>
+    </nav>
+)
+
+export default Nav;
